2c911765944a61ed21e0b93dbe07e048eed392cc
s2422092 <2422092@stu.musashino-u.ac.jp>
2025-06-20 15:52:16 +0900

    説明

---
+++ practice/psql/users.sql
@@ -0,0 +1,6 @@
+-----主キーはid INTEGER PRIMARY KEY AUTOINCREMENT,これにする
+--ここにおいてデータベースの編集するにはまずpracticeディレクトリに移動する必要がある
+-- その後、psqlを起動する
+-- そこでsqlite3 app.dbこのコマンドを実行する
+-- するとsqliteのプロンプトが表示されるので、そこでSQL文を実行する
+-- 例えば、CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, nameなどなど
